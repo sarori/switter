24b98916cbfd87e85e3429c6fe169e3fb96fd36d
sarori <salo3678@gmail.com>
2020-11-28 01:01:40 +0100

    .

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3ZyNykZpVT9KjPlPVzMf8wjDKdvvzyLI",
+    authDomain: "switter-cca2f.firebaseapp.com",
+    databaseURL: "https://switter-cca2f.firebaseio.com",
+    projectId: "switter-cca2f",
+    storageBucket: "switter-cca2f.appspot.com",
+    messagingSenderId: "6590613847",
+    appId: "1:6590613847:web:5f6defb702b352a7305612"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
