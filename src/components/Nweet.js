4da6b8842847e6a4ce9da68f9a1a58532af78336
sarori <salo3678@gmail.com>
2020-12-26 22:26:29 +0100

    .

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React from "react";
+
+const Nweet = ({nweetObj, isOwner}) => (
+    <div>
+        <h4>{nweetObj.text}</h4>
+        {isOwner && (
+            <>
+                <button>Delete Nweet</button>
+                <button>Edit Nweet</button>
+            </>
+        )}
+        
+    </div>
+);
+
+export default Nweet;
